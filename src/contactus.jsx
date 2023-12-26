ce84bdf096fe376e6e26e30ff708401c63441cb4
Arjun Singh <70803514+arjunsingh27@users.noreply.github.com>
2023-12-26 16:42:00 +0530

    Update contactus.jsx

--- src/contactus.jsx
+++ src/contactus.jsx
@@ -2,25 +2,15 @@ import React from "react";
 import './contactus.css';
 
 function ContactUs() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const formData = new FormData(event.target);
-
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams(formData).toString(),
-        })
-        .then(() => alert("Thank you for your submission"))
-        .catch((error) => alert(error));
-    };
-
     return (
         <div className="contactus_container">
             <div className="contactus_inner_container">
                 <h3>Contact Us</h3>
-                <form onSubmit={handleSubmit}>
+                <form name="contactus v1"
+                    method="post"
+                    data-netlify="true"
+                    onSubmit="submit"
+                    >
                     <label htmlFor="name">Name:</label>
                     <br />
                     <input type="text" id="name" name="name" required /><br />
@@ -31,7 +21,7 @@ function ContactUs() {
                     <label htmlFor="message">Message:</label><br />
                     <textarea id="message" name="message" rows="4" required></textarea><br />
                     
-                    <button type="submit">Send</button>
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         </div>
