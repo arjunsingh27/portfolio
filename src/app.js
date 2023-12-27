a4fd59c7432af18ef64af734cf016d5d99a35936
Arjun Singh <70803514+arjunsingh27@users.noreply.github.com>
2023-12-27 10:11:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import Home from "./home.jsx";
 import About from "./about.jsx";
 import Project from "./project.jsx";
 import Contactus from "./contactus.jsx";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
@@ -12,18 +12,22 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
-        <Route path="/home">
-        <Home />
-        </Route>
-        <Route path="/about">
-        <About />
-        </Route>
-        <Route path="/projects">
-        <Project />
-        </Route>
-        <Route path="/contactus">
-        <Contactus />
-        </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Project />
+          </Route>
+          <Route path="/contactus">
+            <Contactus />
+          </Route>
+          {/* Default Route - Redirect to Home */}
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
         </Switch>
       </div>
     </Router>
